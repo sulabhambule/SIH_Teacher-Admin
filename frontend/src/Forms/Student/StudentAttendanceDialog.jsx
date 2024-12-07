6b71e0cfb6b96584bf75afd512892a6b83b7fd34
himanshu mishra <himanshu.mishra0913@gmail.com>
2024-12-07 16:55:51 +0530

    did connection for attendance

--- frontend/src/Forms/Student/StudentAttendanceDialog.jsx
+++ frontend/src/Forms/Student/StudentAttendanceDialog.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState ,useEffect} from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import {
   Dialog,
@@ -18,6 +19,30 @@ const StudentAttendanceDialog = ({
   setSelectedStudents,
   lectureId,
 }) => {
+  const {subjectId} = useParams();
+  console.log("subjectId", subjectId);
+
+  const [attendanceData, setAttendanceData] = useState([]);
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        const token = sessionStorage.getItem("teacherAccessToken");
+        const response = await axios.get(
+          `http://localhost:6005/api/v1/lecture/${subjectId}/students`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        console.log("response data:",response.data);
+        setAttendanceData(response.data.data.students || []);
+      } catch (error) {
+        console.error("Error fetching students:", error);
+      }
+    };
+    fetchStudents();
+  }, [subjectId]);
+  
   const handleMarkAttendance = async () => {
     if (!lectureId) {
       alert("Lecture not added yet!");
@@ -59,7 +84,7 @@ const StudentAttendanceDialog = ({
         </DialogHeader>
         <div className="mt-4">
           <StudentAttendanceTable
-            students={students}
+            students={attendanceData}
             selectedStudents={selectedStudents}
             setSelectedStudents={setSelectedStudents}
           />
