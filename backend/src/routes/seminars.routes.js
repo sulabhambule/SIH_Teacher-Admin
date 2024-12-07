7d4d324f934d61fecc3b3c50600dc3feb66dd494
himanshu mishra <himanshu.mishra0913@gmail.com>
2024-12-07 13:37:15 +0530

    seminar controllers

--- backend/src/routes/seminars.routes.js
+++ backend/src/routes/seminars.routes.js
@@ -1,4 +1,3 @@
-// // routes/seminar.routes.js
 import express from "express";
 import {
   uploadConductedSeminar,
@@ -10,67 +9,79 @@ import {
   getFeedbackSubmitters,
   markAttendance,
   viewSeminarFeedbackFormsAvailable,
+  fillEligibleFeedbackForm,
+  getStudentsByBranch,
 } from "../controllers/seminars.controllers.js";
 import { verifyTeacherJWT } from "../middleware/teacher.auth.middleware.js";
-import {verifyStudentJWT} from "../middleware/student.auth.middleware.js";
+import { verifyStudentJWT } from "../middleware/student.auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
 
 const router = express.Router();
 
-// Teacher Routes
+
 router.post(
   "/seminars/conducted",
   verifyTeacherJWT,
   upload.single("report"),
   uploadConductedSeminar
-); // Upload a conducted seminar
+); 
 
 router.get(
-  "/seminars/conducted",
+  "/conducted",
   verifyTeacherJWT,
   getConductedSeminars
-); // Get all conducted seminars
+); 
 
 router.put(
-  "/seminars/:seminarId",
+  "/:seminarId/edit",
   verifyTeacherJWT,
   editUploadedSeminar
-); // Edit an uploaded seminar
+); 
 
 router.delete(
-  "/seminars/:seminarId",
+  "/:seminarId",
   verifyTeacherJWT,
   deleteUploadedSeminar
-); // Delete an uploaded seminar
+); 
 
 router.post(
-  "/seminars/:seminarId/release-feedback",
+  "/:seminarId/release-feedback",
   verifyTeacherJWT,
   releaseFeedbackForm
-); // Release feedback forms
+); 
 
 router.get(
-  "/seminars/:seminarId/feedbacks",
+  "/:seminarId/feedbacks",
   verifyTeacherJWT,
   viewFeedbacks
-); // View all feedbacks (anonymous)
+); 
 
 router.get(
-  "/seminars/:seminarId/feedback-submitters",
+  "/:seminarId/feedback-submitters",
   verifyTeacherJWT,
   getFeedbackSubmitters
-); // Get feedback submitters list
+); 
 
 router.post(
-  "/seminars/:seminarId/attendance",
+  "/:seminarId/attendance/:teacherId",
   verifyTeacherJWT,
   markAttendance
-); // Mark attendance for a seminar
+); 
+
 
 router.get(
-  "/seminars/feedbacks/available",
+  "/feedbacks-available",
   verifyStudentJWT,
   viewSeminarFeedbackFormsAvailable
-); // View feedback forms available for a student
+); 
+
+router.post(
+  "/:seminarId/fill-feedback",
+  verifyStudentJWT,
+  fillEligibleFeedbackForm
+); 
+
+
+router.get("/students/:branchName", getStudentsByBranch);
 
 export default router;
